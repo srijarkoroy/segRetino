c5c31e5149118fadd7dda77f130dea4a8488c1d2
SaishaShetty <saisha892001@gmail.com>
2021-09-10 23:15:18 +0530

    Data Augmentation file added

---
+++ data/augment.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import cv2
+from glob import glob
+from tqdm import tqdm
+import imageio
+from albumentations import HorizontalFlip, VerticalFlip, Rotate
+
+""" Create a directory """
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def load_data(path):
+    ### Split data into train and test set
+    train_x = sorted(glob(os.path.join(path, "training", "images", "*.tif")))
+    train_y = sorted(glob(os.path.join(path, "training", "1st_manual", "*.gif")))
+
+    test_x = sorted(glob(os.path.join(path, "test", "images", "*.tif")))
+    test_y = sorted(glob(os.path.join(path, "test", "1st_manual", "*.gif")))
+
+    return (train_x, train_y), (test_x, test_y)
+
+def augment_data(images, masks, save_path, augment=True):
+    ### Augmenting data into Horizontal,Vertical & Rotate
+
+    size = (512, 512)
+
+    for idx, (x, y) in tqdm(enumerate(zip(images, masks)), total=len(images)):
+        """ Extracting the name """
+        name = x.split("/")[-1].split(".")[0]
+
+        """ Reading image and mask """
+        x = cv2.imread(x, cv2.IMREAD_COLOR)
+        y = imageio.mimread(y)[0]
+
+        if augment == True:
+            aug = HorizontalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x1 = augmented["image"]
+            y1 = augmented["mask"]
+
+            aug = VerticalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x2 = augmented["image"]
+            y2 = augmented["mask"]
+
+            aug = Rotate(limit=45, p=1.0)
+            augmented = aug(image=x, mask=y)
+            x3 = augmented["image"]
+            y3 = augmented["mask"]
+
+            X = [x, x1, x2, x3]
+            Y = [y, y1, y2, y3]
+
+        else:
+            X = [x]
+            Y = [y]
+
+        index = 0
+        for i, m in zip(X, Y):
+            i = cv2.resize(i, size)
+            m = cv2.resize(m, size)
+
+            tmp_image_name = f"{name}_{index}.png"
+            tmp_mask_name = f"{name}_{index}.png"
+
+            image_path = os.path.join(save_path, "image", tmp_image_name)
+            mask_path = os.path.join(save_path, "mask", tmp_mask_name)
+
+            cv2.imwrite(image_path, i)
+            cv2.imwrite(mask_path, m)
+
+            index += 1
+
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+
+    """ Load the data """
+    data_path = "/Users/saishashetty/Desktop/segRetino/datasets"
+    (train_x, train_y), (test_x, test_y) = load_data(data_path)
+
+    print(f"Train: {len(train_x)} - {len(train_y)}")
+    print(f"Test: {len(test_x)} - {len(test_y)}")
+
+    """ Create directories to save the augmented data """
+    create_dir("/Users/saishashetty/Desktop/segRetino/new_data/train/image")
+    create_dir("/Users/saishashetty/Desktop/segRetino/new_data/train/mask")
+    create_dir("/Users/saishashetty/Desktop/segRetino/new_data/test/image/")
+    create_dir("/Users/saishashetty/Desktop/segRetino/new_data/test/mask/")
+
+    """ Data augmentation """
+    augment_data(train_x, train_y, "/Users/saishashetty/Desktop/segRetino/new_data/train", augment=True)
+    augment_data(test_x, test_y, "/Users/saishashetty/Desktop/segRetino/new_data/test", augment=False)

e4f6e6b6bd1b5921d0ed1e899074f6b0fbdd0110
Srijarko Roy <66861243+srijarkoroy@users.noreply.github.com>
2021-09-13 14:29:43 +0530

    added test file

---
+++ test.py
@@ -0,0 +1,6 @@
+from inference import SegRetino
+
+# Initializing the SegRetino Inference
+
+seg = SegRetino(<path/to/test/img.png>)
+seg.inference(set_weight_dir = 'unet.pth', set_gen_dir = 'result_img')
